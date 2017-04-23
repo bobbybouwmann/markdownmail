0e0f761600f6a608c15c8a2ea326ec2e9ca3c0b4
Bobby Bouwmann <bobbybouwmann@gmail.com>
2017-04-23 22:45:54 +0200

    Add message when no themes are created

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -6,11 +6,37 @@
 
         <div class="themes">
 
-            @foreach ($themes as $theme)
+            @if (count($themes))
 
-                @include('partials.theme')
+                @foreach ($themes as $theme)
 
-            @endforeach
+                    @include('partials.theme')
+
+                @endforeach
+
+            @else
+
+                <div class="theme">
+
+                    <div class="panel">
+
+                        <div class="panel-heading">
+
+                            <h4>It looks like there are no themes yet!</h4>
+
+                        </div>
+
+                        <div class="panel-body">
+
+                            <a href="{{ route('themes.create') }}" class="btn btn-primary">Create My First Theme</a>
+
+                        </div>
+
+                    </div>
+
+                </div>
+
+            @endif
 
         </div>
 
