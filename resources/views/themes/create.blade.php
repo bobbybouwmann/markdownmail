bfabda73a7daed3823f27d6c1c5e8989c0a28995
Bobby Bouwmann <bobbybouwmann@gmail.com>
2017-04-28 15:58:29 +0200

    Keep old input

--- resources/views/themes/create.blade.php
+++ resources/views/themes/create.blade.php
@@ -18,7 +18,7 @@
 
                             <div class="form-group {{ $errors->has('name') ? 'has-error' : '' }}">
                                 <label for="name">Theme name</label>
-                                <input type="text" class="form-control" id="name" name="name" placeholder="Theme name">
+                                <input type="text" class="form-control" id="name" name="name" placeholder="Theme name" value="{{ old('name') }}">
                                 @if ($errors->has('name'))
                                     <span class="help-block">{{ $errors->first('name') }}</span>
                                 @endif
@@ -27,7 +27,7 @@
                             <div class="form-group {{ $errors->has('description') ? 'has-error' : '' }}">
                                 <label for="description">Theme description</label>
                                 <textarea class="form-control" id="description" name="description"
-                                          placeholder="Theme description"></textarea>
+                                          placeholder="Theme description">{{ old('description') }}</textarea>
                                 @if ($errors->has('description'))
                                     <span class="help-block">{{ $errors->first('description') }}</span>
                                 @endif
